5943da73b4389c4a267f59029e5430893e6a2ee8
Gabriel Berdejo <rev-x16.sassorii-wf@hotmail.com>
2021-05-07 00:56:27 -0500

    agragando un servio al pet

--- controllers/pet.controller.js
+++ controllers/pet.controller.js
@@ -2,27 +2,29 @@ const petService = require("../services/pet.service");
 
 const petController = {};
 
-petController.renderMyPets = async (req, res) => {
-  const pets = await petService.getListPetbyClient(req.user.id);
-  res.render("pet_list", { pets });
+petController.renderMyPets = async(req, res) => {
+    const pets = await petService.getListPetbyClient(req.user.id);
+    res.render("pet_list", { pets });
 };
 petController.renderRegisterPet = (req, res) => res.render("pet_register");
 
-petController.createPet = async (req, res) => {
-  const { name, race, sex, birthdata } = req.body;
-  const pet = await petService.createPet({
-    name,
-    race,
-    sex,
-    birthdata,
-    img: "img",
-    clientId: req.user.id,
-  });
+petController.createPet = async(req, res) => {
+    console.log(req.body);
+    const { name, race, sex, birthdata } = req.body;
+    const pet = await petService.createPet({
+        name,
+        race,
+        sex,
+        birthdata,
+        img: "img",
+        clientId: req.user.id,
+    });
 
-  console.log(pet);
-  if (pet) return res.redirect("/my-pets");
+    console.log(pet);
+    if (pet) return res.json(pet)
 
-  return res.redirect("/register-my-pets");
+    //return res.redirect("/register-my-pets");
+    return res.json({ message: "Ocurrio algo" });
 };
 
 module.exports = petController;

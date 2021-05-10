d785838c39a285ef38fbc61c0646eb875393d6b9
Gabriel Berdejo <rev-x16.sassorii-wf@hotmail.com>
2021-05-10 15:13:55 -0500

    creando la inteface del veterinary

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const router = Router();
 const authController = require("../controllers/auth.controller");
+const { isAuthenticated } = require('../helpers');
 router.get("/", authController.renderHome);
 router.get("/login", authController.renderLogin);
 router.post("/login", authController.login);
 router.get("/signup", authController.renderSignUp);
 router.post("/signup", authController.signUp);
-router.get("/exit", authController.logout);
+router.get("/exit", [isAuthenticated], authController.logout);
 
 //employees
 router.get('/signup-admin', authController.renderSignUpAdmin);
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,12 +2,13 @@ const { Router } = require("express");
 const router = Router();
 const productController = require("../controllers/product.controller");
 
-router.get("/list-product", productController.renderListProduct);
-router.get("/create-product", productController.renderCreateProduct);
-router.post("/products", productController.createProduct);
-router.post("/delete-product/:id",productController.deleteProduct);
-router.get("/update-product/:id",productController.renderUpdateProduct);
-router.post("/update-product/:id",productController.updateProduct);
+const { isAuthenticated } = require('../helpers');
+router.get("/list-product", [isAuthenticated], productController.renderListProduct);
+router.get("/create-product", [isAuthenticated], productController.renderCreateProduct);
+router.post("/products", [isAuthenticated], productController.createProduct);
+router.post("/delete-product/:id", [isAuthenticated], productController.deleteProduct);
+router.get("/update-product/:id", [isAuthenticated], productController.renderUpdateProduct);
+router.post("/update-product/:id", [isAuthenticated], productController.updateProduct);
 
 
 module.exports = router;

6d7f3c05833754711e60fe28d0ce04a5b5ca3c77
Gabriel Berdejo <rev-x16.sassorii-wf@hotmail.com>
2021-05-20 12:52:38 -0500

    configurando de las variables de entorno

--- database/index.js
+++ database/index.js
@@ -1,9 +1,21 @@
+const { Sequelize } = require("sequelize");
 
-let sequelize;
-if(process.env.NODE_ENV === 'development'){
-  sequelize = require('./db-development');
-}
-if(process.env.NODE_ENV === 'production'){
-  sequelize = require('./db-production');
-}
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'mikasapet',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASS || 'mysql', {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        logging: false,
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: process.env.DB_SSL || false
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+    }
+);
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const config = require('config');
 const app = require("./app");
 const sequelize = require('./database/index');
 global.DOMAIN = "mikasa.pet";
@@ -8,7 +7,7 @@ sequelize.authenticate()
     .then(() => {
         console.log('Go DataBase!');
         app.listen(process.env.PORT, () => {
-            console.log("Run Server on port: " + config.get('server.PORT'));
+            console.log("Run Server on port: " + process.env.PORT);
         });
     })
     .catch((err) => {

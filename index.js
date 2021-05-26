022c971eff621053fe3377cbf999784bd6c00ea8
Gabriel Berdejo <rev-x16.sassorii-wf@hotmail.com>
2021-05-26 00:20:05 -0500

    set dotenv2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-if (process.env.NODE_ENV === 'development') require("dotenv").config();
+if (process.env.NODE_ENV === 'development') {
+    require("dotenv").config();
+}
 const app = require("./app");
 const sequelize = require('./database/index');
 

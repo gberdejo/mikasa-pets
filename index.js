fafe0b2dfdf08124a90d899e036f5a8005313d17
Gabriel <rev-x16.sassorii-wf@hotmail.com>
2021-06-26 10:27:59 -0500

    haciendo el deploy a heroku

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const app = require("./app");
-const sequelize = require("./database/index");
-const config = require("config");
-const fs = require("fs");
+// require("dotenv").config();
+const app = require('./app');
+const sequelize = require('./database/index');
+const config = require('config');
+const fs = require('fs');
 
 //sequelize.sync({ force: true }).then(() => console.log('sync go!'));
 
@@ -10,16 +10,16 @@ sequelize
   .authenticate()
   .then(() => {
     //sequelize.sync({ alter: true });
-    console.log("Go DataBase!");
-    const server = app.listen(app.get("port"), () => {
-      console.log("Run Server on port: " + app.get("port"));
-      fs.access(config.get("path.uploads"), (err) => {
+    console.log('Go DataBase!');
+    const server = app.listen(app.get('port'), () => {
+      console.log('Run Server on port: ' + app.get('port'));
+      fs.access(config.get('path.uploads'), (err) => {
         if (err) {
-          fs.mkdir(config.get("path.uploads"), (err) => {
+          fs.mkdir(config.get('path.uploads'), (err) => {
             if (!err) {
-              console.log("FOLDER Uploads CREATED");
-              fs.mkdir(config.get("path.edits"), (err) => {
-                if (!err) console.log("FOLDER EDITS CREATED");
+              console.log('FOLDER Uploads CREATED');
+              fs.mkdir(config.get('path.edits'), (err) => {
+                if (!err) console.log('FOLDER EDITS CREATED');
               });
             }
           });

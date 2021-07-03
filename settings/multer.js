07c845b79a8ba3721bb927457f23d9747a3f7a60
Gabriel Berdejo <gabriel.berdejo@holinsys.pe>
2021-07-03 09:14:02 -0500

    ultimo commit

--- settings/multer.js
+++ settings/multer.js
@@ -2,21 +2,23 @@ const multer = require('multer');
 const config = require('config');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-
-        cb(null, config.get('path.uploads'))
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, config.get('path.uploads'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'image/jpeg' ||file.mimetype == 'image/jpg'|| file.mimetype == 'image/png') {
-        cb(null, true);
-    } else {
-
-        cb(null, false);
-    }
-}
+  if (
+    file.mimetype == 'image/jpeg' ||
+    file.mimetype == 'image/jpg' ||
+    file.mimetype == 'image/png'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 const upload = multer({ storage, fileFilter });
 module.exports = upload;
